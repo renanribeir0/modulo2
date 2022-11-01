b901c38a1beb83d13f4203fb89f1bd62e13acc40
renanribeir0 <110369271+renanribeir0@users.noreply.github.com>
2022-11-01 14:12:10 -0300

    entrega modelo relacional
    entrega

---
+++ 02_AUT_EST_ENTREGA/Semana 3/app_30.js
@@ -0,0 +1,114 @@
+const express = require('express'); 
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
+
+    app.use(express.json());
+    app.get('/Formacao', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'id_formacao, Curso, Duracao, DescricaoDoCargo FROM formacao ORDER BY id_formacao DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/Experiencia', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT id_experiencia, NomeDaEmpresa, Cargo, Duracao, DescricaoDaFuncao, id_sobremim FROM formacao ORDER BY id_experiencia DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/Habilidades', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT id_habilidades, DescrevaSuasHabilidades, id_sobremim FROM formacao ORDER BY id_habilidades DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/Personalidade', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT id_personalidade, QuaisSeusPrincipaiPontosFortes, id_sobremim, FROM formacao ORDER BY id_personalidade DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/Realizacoes', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'id_realizacoes, OQueFez, Quando, id_sobremim FROM formacao ORDER BY id_realizacoes DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/SobreMim', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'id_sobremim, endereco, telefone, email, MinhaTrajetoria, foto, cargo FROM formacao ORDER BY id_sobremim DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.use(express.json());
+    app.get('/SuaFoto', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'id_SuaFoto, SeuNome, telefone, Seucargo, id_sobremim FROM formacao ORDER BY id_SuaFoto DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.listen(3000)
